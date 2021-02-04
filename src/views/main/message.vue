fc8c557ce7cfa95bf8904aed53158e102472a400
DiaryKyosuke <fendianwar36@gmail.com>
2021-02-04 12:50:05 +0700

    update chat responsive on mobile

--- src/views/main/message.vue
+++ src/views/main/message.vue
@@ -1,141 +1,143 @@
 <template>
 <div>
-      <!-- this friends detail -->
-  <div ref="friendsDetail" class="info-friends ps-2">
-    <div class="sub-friends-detail">
-      <div class="header row p-3" style="color: #7E98DF;">
-        <div class="col login-name d-flex align-items-center justify-content-center" style="position:relative;">
-          <i style="cursor:pointer; position:absolute; left: 100px; top: 6px;" @click="closeNav" class="fas fa-chevron-left fa-lg"></i>
-          <h5>@{{friendsInfo.username}}</h5>
+    <!-- this friends detail -->
+    <div ref="friendsDetail" class="info-friends">
+        <div class="sub-friends-detail">
+        <div class="header row p-3" style="color: #7E98DF;">
+            <div class="col login-name d-flex align-items-center justify-content-center" style="position:relative;">
+            <i style="cursor:pointer; position:absolute; left: 100px; top: 6px;" @click="closeNav" class="fas fa-chevron-left fa-lg"></i>
+            <h5>@{{friendsInfo.username}}</h5>
+            </div>
         </div>
-      </div>
-      <div class="row mt-2">
-       <div class="col d-flex justify-content-center align-items-center">
-         <label class="d-flex justify-content-center align-items-center" style="border-radius:100%; width:80px; height:80px; overflow:hidden; cursor:pointer;">
-           <img :src="friendsInfo.images" alt="" height="80px">
-         </label>
-       </div>
-     </div>
-     <div class="row p-3 mt-3">
-      <div class="col">
-        <h6 style="font-size: 18px">{{friendsInfo.name}}</h6>
-        <h6 style="font-size: 12px; font-weight: 400; color: #7E98DF;" v-if="cek.includes(friendsInfo.id) === true" >Online</h6>
-        <h6 style="font-size: 12px; font-weight: 400; color: #7E98DF;" v-else>Terlihat {{ $dayjs(friendsInfo.updatedAt).fromNow() }}</h6>
-      </div>
-      <div class="col d-flex justify-content-end align-items-center">
-        <i class="fas fa-comment-alt fa-lg"></i>
-      </div>
+        <div class="row mt-2">
+        <div class="col d-flex justify-content-center align-items-center">
+            <label class="d-flex justify-content-center align-items-center" style="border-radius:100%; width:80px; height:80px; overflow:hidden; cursor:pointer;">
+            <img :src="friendsInfo.images" alt="" height="80px">
+            </label>
+        </div>
+        </div>
+        <div class="row p-3 mt-3">
+        <div class="col">
+            <h6 style="font-size: 18px">{{friendsInfo.name}}</h6>
+            <h6 style="font-size: 12px; font-weight: 400; color: #7E98DF;" v-if="cek.includes(friendsInfo.id) === true" >Online</h6>
+            <h6 style="font-size: 12px; font-weight: 400; color: #7E98DF;" v-else>Terlihat {{ $dayjs(friendsInfo.updatedAt).fromNow() }}</h6>
+        </div>
+        <div class="col d-flex justify-content-end align-items-center">
+            <i class="fas fa-comment-alt fa-lg"></i>
+        </div>
+        </div>
+        <div class="row p-2 ps-3">
+        <div>
+            <h6 style="font-size: 18px">Phone number</h6>
+            <h6 style="font-size: 16px; font-weight: 400;">{{friendsInfo.phone || '-'}}</h6>
+        </div>
+        </div>
+        <hr>
+        <div class="row p-3">
+        <ul class="nav nav-pills mb-3 d-flex justify-content-around" id="pills-tab" role="tablist" >
+            <li class="nav-item" role="presentation">
+            <a class="nav-link active" id="pills-location-tab" data-bs-toggle="pill" href="#pills-location" role="tab" aria-controls="pills-location" aria-selected="true">Location</a>
+            </li>
+            <li class="nav-item" role="presentation">
+            <a class="nav-link" id="pills-images-tab" data-bs-toggle="pill" href="#pills-images" role="tab" aria-controls="pills-images" aria-selected="false">Images</a>
+            </li>
+            <li class="nav-item" role="presentation">
+            <a class="nav-link" id="pills-ducuments-tab" data-bs-toggle="pill" href="#pills-ducuments" role="tab" aria-controls="pills-ducuments" aria-selected="false">Ducuments</a>
+            </li>
+        </ul>
+        <div class="tab-content" id="pills-tabContent" style="overflow-y: scroll; overflow-x: hidden; height:36vh;">
+            <div class="tab-pane fade show active" id="pills-location" role="tabpanel" aria-labelledby="pills-location-tab">
+            <l-map :zoom="zoom" :center="friendCenter" style="height: 200px; width: 100%">
+                <l-tile-layer :url="url" :attribution="attribution" />
+                <l-marker :lat-lng="friendMarkerLatLng"></l-marker>
+            </l-map>
+            </div>
+            <div class="tab-pane fade" id="pills-images" role="tabpanel" aria-labelledby="pills-images-tab">
+            images
+            </div>
+            <div class="tab-pane fade" id="pills-ducuments" role="tabpanel" aria-labelledby="pills-ducuments-tab">
+            this document
+            </div>
+        </div>
+        </div>
     </div>
-    <div class="row p-2 ps-3">
-      <div>
-        <h6 style="font-size: 18px">Phone number</h6>
-        <h6 style="font-size: 16px; font-weight: 400;">{{friendsInfo.phone || '-'}}</h6>
-      </div>
     </div>
-    <hr>
-    <div class="row p-3">
-      <ul class="nav nav-pills mb-3 d-flex justify-content-around" id="pills-tab" role="tablist" >
-        <li class="nav-item" role="presentation">
-          <a class="nav-link active" id="pills-location-tab" data-bs-toggle="pill" href="#pills-location" role="tab" aria-controls="pills-location" aria-selected="true">Location</a>
-        </li>
-        <li class="nav-item" role="presentation">
-          <a class="nav-link" id="pills-images-tab" data-bs-toggle="pill" href="#pills-images" role="tab" aria-controls="pills-images" aria-selected="false">Images</a>
-        </li>
-        <li class="nav-item" role="presentation">
-          <a class="nav-link" id="pills-ducuments-tab" data-bs-toggle="pill" href="#pills-ducuments" role="tab" aria-controls="pills-ducuments" aria-selected="false">Ducuments</a>
-        </li>
-      </ul>
-      <div class="tab-content" id="pills-tabContent" style="overflow-y: scroll; overflow-x: hidden; height:36vh;">
-        <div class="tab-pane fade show active" id="pills-location" role="tabpanel" aria-labelledby="pills-location-tab">
-          <l-map :zoom="zoom" :center="friendCenter" style="height: 200px; width: 100%">
-            <l-tile-layer :url="url" :attribution="attribution" />
-            <l-marker :lat-lng="friendMarkerLatLng"></l-marker>
-          </l-map>
+    <!-- end  friends detail -->
+    <div class="row" style="margin:0;">
+        <div class="col">
+            <div class="row chaters">
+            <div class=" col-1 d-flex align-items-start chat-images">
+                <label class="d-flex justify-content-center align-items-center" style="border-radius:100%; width:70px; height:70px; overflow:hidden; cursor:pointer;">
+                <img :src="friendsInfo.images" alt="" height="60px">
+                </label>
+            </div>
+            <div class="col d-flex align-items-center">
+                <div class="row">
+                <div class="col-12 text-dark name">
+                    {{friendsInfo.name}}
+                </div>
+                <div class="col-12 status">
+                    <span v-if="cek.includes(friendsInfo.id) === true" >Online</span>
+                    <span v-else>Terlihat {{ $dayjs(friendsInfo.updatedAt).fromNow() }}</span>
+                </div>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-end align-items-center">
+            <i @click="openNav" class="fas fa-th-large fa-lg" style=" color: #7E98DF; cursor:pointer;"></i>
+            </div>
         </div>
-        <div class="tab-pane fade" id="pills-images" role="tabpanel" aria-labelledby="pills-images-tab">
-          images
+        <div class="row" style="background:#EDEDED; padding-right: 0 !important;">
+            <div ref="messageBody" style="width:100%; height: 66.5vh; overflow-y: scroll; overflow-x: hidden; position: relative; padding: 18px; padding-right: 0;">
+            <div v-for="(message, index ) in messages" :key="index">
+                <!-- ini untuk penerima -->
+                <div class="row mb-3" v-if="message.status === 'dikirim' || message.receiverId == idLogin">
+                <div class="chat-images col-1 d-flex align-items-end">
+                    <label class="d-flex justify-content-center align-items-center" style="border-radius:100%; width:50px; height:50px; overflow:hidden; cursor:pointer;">
+                    <img :src="friendsInfo.images" alt="" height="50px">
+                    </label>
+                </div>
+                <div class="list-message col-auto d-flex justify-content-end align-items-end" style="max-width: 35%;">
+                    <div class="chat-receiver">
+                    {{message.messages}}
+                    </div>
+                </div>
+                <div class="col d-flex align-items-center textDateReceiver">
+                    {{ $dayjs(message.date).calendar(null, { sameDay: '[Hari ini] HH:mm', lastDay: '[Kemarin ] HH:mm', lastWeek: ' ddd DD HH:mm', sameElse: 'DD/MM/YYYY' }) }}
+                </div>
+                </div>
+                <!-- ini untuk pengirim -->
+                <div v-else class="row mb-3">
+                <div class="col d-flex align-items-center justify-content-end textDateSender">
+                    {{ $dayjs(message.date).calendar(null, { sameDay: '[Hari ini] HH:mm', lastDay: '[Kemarin ] HH:mm', lastWeek: ' ddd DD HH:mm', sameElse: 'DD/MM/YYYY' }) }}
+                </div>
+                <div class="list-message col-auto d-flex justify-content-end align-items-end" style="max-width: 35%;">
+                    <div class="chat-sender">
+                    {{message.messages}}
+                    </div>
+                </div>
+                <div class="chat-images col-1 d-flex align-items-end" style="text-align:right;">
+                    <label class="d-flex justify-content-center align-items-center" style="border-radius:100%; width:50px; height:50px; overflow:hidden; cursor:pointer;">
+                    <img :src="dataPersonal.images" alt="" height="50px">
+                    </label>
+                </div>
+                </div>
+            </div>
+
+            </div>
         </div>
-        <div class="tab-pane fade" id="pills-ducuments" role="tabpanel" aria-labelledby="pills-ducuments-tab">
-          this document
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- end  friends detail -->
- <div>
-    <div class="row chaters">
-      <div class=" col-1 d-flex align-items-start chat-images">
-        <label class="d-flex justify-content-center align-items-center" style="border-radius:100%; width:70px; height:70px; overflow:hidden; cursor:pointer;">
-          <img :src="friendsInfo.images" alt="" height="60px">
-        </label>
-      </div>
-      <div class="col d-flex align-items-center">
+
         <div class="row">
-          <div class="col-12 text-dark name">
-            {{friendsInfo.name}}
-          </div>
-          <div class="col-12 status">
-            <span v-if="cek.includes(friendsInfo.id) === true" >Online</span>
-            <span v-else>Terlihat {{ $dayjs(friendsInfo.updatedAt).fromNow() }}</span>
-          </div>
+        <div class="col position-relative" style="padding: 20px;">
+            <input style="padding-right:150px" v-model="inputMessage" @keypress.enter="sendMessage" class="input-message" type="text" placeholder="Type your message...">
+            <!-- <div class="fa-lg position-absolute fitur-input-message">
+            <i class="fas fa-plus"></i>
+            <i class="fas fa-surprise ms-3"></i>
+            <i class="fas fa-camera ms-3"></i>
+            </div> -->
         </div>
-      </div>
-      <div class="col d-flex justify-content-end align-items-center">
-       <i @click="openNav" class="fas fa-th-large fa-lg" style=" color: #7E98DF; cursor:pointer;"></i>
-     </div>
-   </div>
-   <div class="row" style="background:#EDEDED;">
-     <div ref="messageBody" class="" style=" height:66.5vh; overflow-y: scroll; position:relative; padding:18px;">
-      <div v-for="(message, index ) in messages" :key="index">
-        <!-- ini untuk penerima -->
-        <div class="row mb-3" v-if="message.status === 'dikirim' || message.receiverId == idLogin">
-          <div class="chat-images col-1 d-flex align-items-end">
-            <label class="d-flex justify-content-center align-items-center" style="border-radius:100%; width:50px; height:50px; overflow:hidden; cursor:pointer;">
-              <img :src="friendsInfo.images" alt="" height="50px">
-            </label>
-          </div>
-          <div class="list-message col-auto d-flex justify-content-end align-items-end" style="max-width: 35%;">
-            <div class="chat-receiver">
-              {{message.messages}}
-            </div>
-          </div>
-          <div class="col d-flex align-items-center textDateReceiver">
-            {{ $dayjs(message.date).calendar(null, { sameDay: '[Hari ini] HH:mm', lastDay: '[Kemarin ] HH:mm', lastWeek: ' ddd DD HH:mm', sameElse: 'DD/MM/YYYY' }) }}
-          </div>
         </div>
-        <!-- ini untuk pengirim -->
-        <div v-else class="row mb-3">
-          <div class="col d-flex align-items-center justify-content-end textDateSender">
-            {{ $dayjs(message.date).calendar(null, { sameDay: '[Hari ini] HH:mm', lastDay: '[Kemarin ] HH:mm', lastWeek: ' ddd DD HH:mm', sameElse: 'DD/MM/YYYY' }) }}
-          </div>
-          <div class="list-message col-auto d-flex justify-content-end align-items-end" style="max-width: 35%;">
-            <div class="chat-sender">
-              {{message.messages}}
-            </div>
-          </div>
-          <div class="chat-images col-1 d-flex align-items-end">
-            <label class="d-flex justify-content-center align-items-center" style="border-radius:100%; width:50px; height:50px; overflow:hidden; cursor:pointer;">
-              <img :src="dataPersonal.images" alt="" height="50px">
-            </label>
-          </div>
         </div>
-      </div>
-
     </div>
-  </div>
-
-  <div class="row">
-   <div class="col position-relative" style="padding: 20px;">
-     <input style="padding-right:150px" v-model="inputMessage" @keypress.enter="sendMessage" class="input-message" type="text" placeholder="Type your message...">
-     <!-- <div class="fa-lg position-absolute fitur-input-message">
-       <i class="fas fa-plus"></i>
-       <i class="fas fa-surprise ms-3"></i>
-       <i class="fas fa-camera ms-3"></i>
-     </div> -->
-   </div>
- </div>
-</div>
 </div>
 </template>
 
